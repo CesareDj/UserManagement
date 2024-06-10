5c586a123c5d6d7d5fad92ab3768f041d4948597
César González Alonso <cesaredj@outlook.es>
2024-06-10 19:11:16 +0200

    Add entities.

---
+++ UserManagementAPI/Data/UserManagementDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using UserManagementAPI.Models;
+
+public class UserManagementDbContext : DbContext
+{
+    public UserManagementDbContext(DbContextOptions<UserManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public override int SaveChanges()
+    {
+        var currentTime = DateTime.Now;
+
+        foreach (var entry in ChangeTracker.Entries().Where(e => e.State == EntityState.Added))
+        {
+            if (entry.Entity is BaseEntity entity)
+            {
+                entity.CreatedAt = currentTime;
+            }
+        }
+
+        foreach (var entry in ChangeTracker.Entries().Where(e => e.State == EntityState.Modified))
+        {
+            if (entry.Entity is BaseEntity entity)
+            {
+                entity.UpdatedAt = currentTime;
+            }
+        }
+
+        foreach (var entry in ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted && e.Metadata.GetProperties().Any(x => x.Name == "DeletedAt")))
+        {
+            entry.State = EntityState.Unchanged;
+            entry.CurrentValues["DeletedAt"] = currentTime;
+        }
+
+        return base.SaveChanges();
+    }
+}
---
+++ UserManagementAPI/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagementAPI.Models
+{
+    public class BaseEntity
+    {
+        public string CreatedBy { get; set; } = "ADMIN";
+        public DateTime CreatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? DeletedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+
+}
